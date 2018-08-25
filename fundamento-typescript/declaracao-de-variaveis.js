6dd25c24b623322f801f7a5e51f3d33fce3be94b
Rafael <flns_rafa@hotmail.com>
2018-08-25 17:36:23 -0300

    Declaração de variáveis (var, let e const)

---
+++ fundamento-typescript/declaracao-de-variaveis.js
@@ -0,0 +1,18 @@
+// var, let e const
+// const
+//O IDEAL É SEMPRE USAR LET PARA RESPEITAR O ESCOPO
+//CONST NÃO DEIXA MUDAR O VALOR
+function iniciarTime(iniciaJogoEmUberlandia) {
+    var nome = 'Messi e amigos';
+    var cidade = 'em São Paulo';
+    //var nome = 'Messi e amigos';
+    if (iniciaJogoEmUberlandia) {
+        cidade = 'em Uberlândia';
+        //var cidade = 'em Uberlândia'; mesmo dentro do escopo do if consegue acessar a variável fora lá embaixo, isso por causa do hoisting que declara a variavel lá em cima no começo da função
+        //Já quando usamos let, ela só é válida no escopo declarado, o que faz mais sentido
+    }
+    //nome = 'Ronaldo e amigos'; NÃO FUNCIONA POIS É UMA CONSTANTE
+    // nome = 'Ronaldo e amigos';
+    console.log(nome + " v\u00E3o jogar " + cidade);
+}
+iniciarTime(false);
