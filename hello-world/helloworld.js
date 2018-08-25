aac6f7da6ad414f3ece6b9be1fbefcb7f079c767
Rafael <flns_rafa@hotmail.com>
2018-08-25 15:28:51 -0300

    Primeiro código com typescript

---
+++ hello-world/helloworld.js
@@ -0,0 +1,8 @@
+//String nome -> Java
+//nome: string -> TypeScript
+function dizerOla(nome) {
+    //console.log("Olá " + nome); igual ao debaixo
+    console.log("Ol\u00E1 " + nome);
+}
+dizerOla("Algaworks");
+//dizerOla(4); NÃO COMPILA POR SER STRING
---
+++ hello-world/helloworld.ts
@@ -0,0 +1,10 @@
+//String nome -> Java
+//nome: string -> TypeScript
+
+function dizerOla(nome: string){
+    //console.log("Olá " + nome); igual ao debaixo
+    console.log(`Olá ${nome}`);
+}
+
+dizerOla("Algaworks");
+//dizerOla(4); NÃO COMPILA POR SER STRING
