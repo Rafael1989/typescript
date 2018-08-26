8bbcea7df6cb3a9a4138d228babd2929d122edfd
Rafael <flns_rafa@hotmail.com>
2018-08-26 12:50:30 -0300

    Construindo sua primeira classe

---
+++ orientacao-a-objetos/Cliente.js
@@ -0,0 +1,16 @@
+//SÓ PODE TER UM CONSTRUTOR
+var Cliente = /** @class */ (function () {
+    function Cliente(nome, idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+    Cliente.prototype.apresentar = function () {
+        return "Ol\u00E1, eu sou " + this.nome + " e tenho " + this.idade + " anos";
+    };
+    return Cliente;
+}());
+var joao = new Cliente('João', 25);
+var apresentacao = joao.apresentar();
+console.log(apresentacao);
+var maria = new Cliente('Maria', 23);
+console.log(maria.apresentar());
